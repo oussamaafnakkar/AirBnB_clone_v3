ad6ca8bfcb1d5182397fe2778f3297ea528b2d9e
oussamaafnakkar <oussamaafnakkar2002@gmail.com>
2024-01-06 14:57:46 +0000

    __init

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,19 +1,19 @@
 #!/usr/bin/python3
-'''
-Creates a Blueprint instance with `url_prefix` set to `/api/v1`.
-'''
-
-
+"""
+Initializes the blueprint instance for the RESTful API views
+"""
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
 
+# Import your new view
 from api.v1.views.index import *
 from api.v1.views.states import *
-from api.v1.views.cities import *
 from api.v1.views.amenities import *
+from api.v1.views.cities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
-from api.v1.views.places_reviews import *
-from api.v1.views.places_amenities import *
+from api.v1.views.places_amenities import  # Add this line to import the new view
+
+__all__ = ['app_views']
 
